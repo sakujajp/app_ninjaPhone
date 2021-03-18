2ca3c2b8d1cfefb5bf05d0183c746b51365b4b51
thuy tranvan <sakujajp@gmail.com>
2021-03-18 08:39:49 +0700

    add project

---
+++ appphone.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 800
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
